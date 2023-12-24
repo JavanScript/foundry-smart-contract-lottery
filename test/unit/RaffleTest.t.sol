d84865c6026d94d2c5100f6154f07b0fbfafaa76
JavanScript <hosseinjavanbakht96@gmail.com>
2023-12-24 11:18:13 +0330

    Wrote some tests

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -11,11 +11,45 @@ contract RaffleTest is Test {
     Raffle raffle;
     HelperConfig helperConfig;
 
+    uint256 entranceFee;
+    uint256 interval /*seconds*/;
+    address vrfCoordinator;
+    bytes32 gasLane;
+    uint64 subscriptionId;
+    uint32 callbackGasLimit;
+
     address public PLAYER = makeAddr("player");
     uint256 public constant STARTING_BALANCE = 10 ether;
 
     function setUp() external {
         DeployRaffle deployer = new DeployRaffle();
         (raffle, helperConfig) = deployer.run();
+        (
+            entranceFee,
+            interval /*seconds*/,
+            vrfCoordinator,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+        vm.deal(PLAYER, 999999999999999999999999);
+    }
+
+    function testRaffleInitialzesInOpenState() public view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    // enter raffle
+    function testRaffleRevertsWhenYouDontPayEnough() public{
+        vm.prank(PLAYER);
+        vm.expectRevert(Raffle.Raffle_NotEnoughEthSent.selector);
+        raffle.enterRaffle();
+    }
+
+    function testRaffleRecordsPlayerWhenTheyEnter() public{
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        address playerRecorded = raffle.getPlayer(0);
+        assert(playerRecorded == PLAYER);
     }
 }
